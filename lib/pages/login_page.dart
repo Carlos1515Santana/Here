2036ab25a832e1a846d8c08c8ceada1a9484e8f9
Carlos1515Santana <carlossergio1515@gmail.com>
2020-03-24 16:30:15 -0300

    Implementação das validações do Login;

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:here/pages/home_page.dart';
+import 'package:here/utils/nav.dart';
+import 'package:here/widgets/AppButton.dart';
+import 'package:here/widgets/app_text.dart';
+import 'package:here/utils/nav.dart';
 
-class LoginPage extends StatelessWidget {
-  final _tLogin = TextEditingController();
-  final _tSenha = TextEditingController();
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _tLogin = TextEditingController(text: "ricardo");
+
+  final _tSenha = TextEditingController(text: "123");
+
+  final _focusSenha = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,68 +35,67 @@ class LoginPage extends StatelessWidget {
   }
 
   _body() {
-    return Container(
-      padding: EdgeInsets.all(30),
-      margin: EdgeInsets.only(top: 120),
-      child: ListView(
-        children: <Widget>[
-          _text("Login", "Digite o login", controller: _tLogin),
-          SizedBox(
-              height: 10
-          ),
-          _text("Senha", "Digite a senha", controller: _tSenha, password: true),
-          SizedBox(
-            height: 20,
-          ),
-          _button("Login", _onClickLogin),
-        ],
-      ),
-    );
-  }
-
-  _text(String label, String hint, {bool password = false, TextEditingController controller}) {
-    return TextFormField(
-      controller: controller,
-      obscureText: password,
-      style: TextStyle(
-        fontSize: 25,
-        color: Colors.blue,
-      ),
-      decoration: InputDecoration(
-        labelText: label,
-        labelStyle: TextStyle(
-          fontSize: 25,
-          color: Colors.grey,
+    return Form(
+      key: _formKey,
+      child: Container(
+        padding: EdgeInsets.all(30),
+        margin: EdgeInsets.only(top: 120),
+        child: ListView(
+          children: <Widget>[
+            AppText("Login", "Digite o login",
+                controller: _tLogin,
+                validator: _validateLogin,
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
+                nextFocus: _focusSenha),
+            SizedBox(height: 10),
+            AppText("Senha", "Digite a senha",
+                controller: _tSenha,
+                password: true,
+                validator: _validateSenha,
+                keyboardType: TextInputType.number,
+                focusNode: _focusSenha),
+            SizedBox(
+              height: 20,
+            ),
+            AppButton("Login", _onClickLogin),
+          ],
         ),
-        hintText: hint,
-        hintStyle: TextStyle(
-          fontSize: 16,
-        ),
-      ),
-    );
-  }
-
-  _button(String text, Function onPressed) {
-    return Container(
-      height: 46,
-      child: RaisedButton(
-        color: Colors.blue,
-        child: Text(
-          text,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 22,
-          ),
-        ),
-        onPressed: onPressed,
       ),
     );
   }
 
   void _onClickLogin() {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+
     String login = _tLogin.text;
     String senha = _tSenha.text;
-
     print("Login: $login, Senha: $senha");
+    push(context, HomePage());
+
+  }
+
+  String _validateLogin(String text) {
+    if (text.isEmpty) {
+      return "Digite o login";
+    }
+    return null;
+  }
+
+  String _validateSenha(String text) {
+    if (text.isEmpty) {
+      return "Digite a senha";
+    }
+    if (text.length < 3) {
+      return "A senha precisa ter pelo menos 3 números";
+    }
+    return null;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
