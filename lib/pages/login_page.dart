b76049dfa946e5518e5aaadb5144e284649e7462
Carlos1515Santana <carlossergio1515@gmail.com>
2020-05-04 23:15:06 -0300

    Ajuste basico

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -16,7 +16,7 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
-  final _tLogin = TextEditingController(text: "ricardo");
+  final _tLogin = TextEditingController(text: "user");
   final _tSenha = TextEditingController(text: "123");
   final _focusSenha = FocusNode();
   bool _showProgress = false;
